43f302f23f7b27117a99d433117c3b3a201f8e5b
Administrator <admin@example.com>
2021-06-27 17:06:37 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,70 +7,76 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class AppComponent   {
   title = 'playbookAssignment';
-
-// @ViewChild('dragItem')dragItem !:ElementRef;
-// @ViewChild('dragItem1')dragItem1 !:ElementRef;
-// @ViewChild('playBox')playBox !:ElementRef;
-
-  
  
 constructor(private elementRef:ElementRef) {}
 
 ngAfterViewInit() {
+  //drag slide 
  let dragItem= this.elementRef.nativeElement.querySelector('.drag-item');
      dragItem.addEventListener('dragstart', this.ondragStart.bind(this)); 
      dragItem.addEventListener('dragend', this.onDragEnd.bind(this));  
-
+//drop on playbox
      let playbox= this.elementRef.nativeElement.querySelector('.play-box'); 
      playbox.addEventListener('dragenter', this.ondrageEnter.bind(this)); 
      playbox.addEventListener('dragover', this.onDragOver.bind(this)); 
-     playbox.addEventListener('dragleave', this.ondragLeave.bind(this)); 
-     playbox.addEventListener('drop', this.onDrop.bind(this)); 
-
-    //  let dragItem1= this.elementRef.nativeElement.querySelector('.drag-item1');
-    //  dragItem1.addEventListener('dragstart', this.ondragStart.bind(this)); 
-    //  dragItem1.addEventListener('dragend', this.onDragEnd.bind(this));  
-
-
+     playbox.addEventListener('drop', this.onDrop.bind(this),false); 
+//drag images
+     let dragItem1= this.elementRef.nativeElement.querySelector('.drag-item1');
+     dragItem1.addEventListener('dragstart', this.ondragStart.bind(this)); 
+      
 }
  
-ondragStart(event) {
-  console.log('drag start');
-  event.dataTransfer.setData("text", event.target.id)
-  console.log(event.target.className);
+//fuction for dragStart event
+ondragStart(ev) {
+ // console.log('drag start');
+ // Set the drag's format and data. Use the event target's id for the data
+ ev.dataTransfer.setData("text/plain", ev.target.id);
+ }
+ 
+//function for dragend event
+onDragEnd(ev){
+  // console.log('end');
+ev.currentTarget.style.border = "solid";
+ }
+
+ //function for dragEnter event
+ondrageEnter(event){ 
+  event.target.classList.add("dragEnter")
 }
- 
- 
-
-onDragEnd(event){console.log('end')}
-
-ondrageEnter(event){console.log('enter')}
 
 onDragOver(event){
-  console.log('over');
+  
   event.preventDefault();
 }
 
-ondragLeave(event){console.log('leave')}
-
-onDrop(event){
-   
-  console.log('drop');
-  event.preventDefault();
-  var draggableElementBrand= event.dataTransfer.getData("text");
-  console.log(draggableElementBrand)
- var draggableElement= document.getElementById(draggableElementBrand)
- let t = event.target.classList.add("droppable-hover");
- console.log(t)
- console.log(draggableElementBrand)
-  event.target.insertAdjacentHTML("afterbegin", `<div class="drag-item" id="${draggableElementBrand}" style>hii</div>`)
- 
-}
  
 
+onDrop(ev){
+  ev.preventDefault();
+  // Get the data, which is the id of the drop target
+  var data = ev.dataTransfer.getData("text");
+  var id =document.getElementById(data)
+  //drop element if id is matched to drop element
+  if(data === "yellow" ||"blue" ||"green" || "red"){
+    
+    ev.target.classList.add("dropped");
+    ev.target.classList.add("position");
+   ev.target.appendChild(id);
+ ev.currentTarget.style.background= "droppable-hover";
+ 
+ ev.target.classList.remove("dragEnter")
+  }
+//drop element if target contains dropped classname an get data is match the id of drop element
+ if(ev.target.classList.contains("dropped") && data === "img1" || "img2" || "img3" || "img4" || "img5" || "img6" ){
+   var id =document.getElementById(data)
+  ev.target.appendChild(id);
  
  
- 
+ }
+  
 }
+ 
+ 
+ }
 
  
